56b156e5555e2c0cc618f39ff9b45380d7cf4c91
NorieCabazares <norie.cabazares@evsu.edu.ph>
2021-05-24 16:08:18 +0800

    activity 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJUWYP-sv-42FnHogrCoVmAsPTQ_r0T3M",
+    authDomain: "jessicarosal-4cc52.firebaseapp.com",
+    databaseURL: "https://jessicarosal-4cc52-default-rtdb.firebaseio.com",
+    projectId: "jessicarosal-4cc52",
+    storageBucket: "jessicarosal-4cc52.appspot.com",
+    messagingSenderId: "806089167629",
+    appId: "1:806089167629:web:9139473ccef41bee845644",
+    measurementId: "G-60TSL09Y07"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJUWYP-sv-42FnHogrCoVmAsPTQ_r0T3M",
+    authDomain: "jessicarosal-4cc52.firebaseapp.com",
+    databaseURL: "https://jessicarosal-4cc52-default-rtdb.firebaseio.com",
+    projectId: "jessicarosal-4cc52",
+    storageBucket: "jessicarosal-4cc52.appspot.com",
+    messagingSenderId: "806089167629",
+    appId: "1:806089167629:web:9139473ccef41bee845644",
+    measurementId: "G-60TSL09Y07"
+  }
 };
 
 /*
